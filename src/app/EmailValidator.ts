00d368e4d1a21c4474bf8ae348730a630d346543
mkuster <kuster.maciej@gmail.com>
2017-08-24 19:58:35 +0200

    EmailValidator implementation

---
+++ src/app/EmailValidator.ts
@@ -0,0 +1,8 @@
+export class EmailValidator {
+  public validate(email: string) {
+    /* tslint:disable */
+    const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regExp.test(email);
+    /* tslint:enable */
+  }
+}
